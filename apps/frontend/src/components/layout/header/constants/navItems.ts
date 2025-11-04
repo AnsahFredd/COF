fd5a3ebdd71c7a4a5af2fe290a6da33589977bb3
Ansah Fred <ansfred111@gmail.com>
2025-11-04 19:07:45 +0000

    Updated the navitems by making the 'o' in the our service capital for consistency

--- apps/frontend/src/components/layout/header/constants/navItems.ts
+++ apps/frontend/src/components/layout/header/constants/navItems.ts
@@ -2,7 +2,7 @@ import { ROUTES } from 'src/constants/routes';
 
 export const NAVITEMS = [
   { label: 'Home', href: ROUTES.HOME },
-  { label: 'our Services', href: ROUTES.SERVICES },
+  { label: 'Our Services', href: ROUTES.SERVICES },
   { label: 'Portfolio', href: ROUTES.PORTFOLIO },
   { label: 'About Us', href: ROUTES.ABOUT },
   { label: 'Booking', href: ROUTES.BOOKING },
